44d9096d10215314bd7232b86061a724e70ce6ee
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2020-10-19 20:08:29 -0300

    Data context - Entity Framework

---
+++ Northwind-desktop-app/Data/NorthwindContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Northwind_desktop_app.Data
+{
+    class NorthwindContext : DbContext
+    {
+        public NorthwindContext() : base("Northwind")
+        {
+        }
+    }
+}
--- Northwind-desktop-app/Program.cs
+++ Northwind-desktop-app/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+using Northwind_desktop_app.Data;
 
 namespace Northwind_desktop_app
 {
@@ -10,6 +9,9 @@ namespace Northwind_desktop_app
     {
         static void Main(string[] args)
         {
+            NorthwindContext db = new NorthwindContext();
+            
         }
     }
+        
 }
