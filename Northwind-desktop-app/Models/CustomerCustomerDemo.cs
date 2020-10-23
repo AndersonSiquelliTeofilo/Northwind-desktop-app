1940029be1bbfc8110f5316293c1604517b3885a
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2020-10-22 21:45:58 -0300

    Customer, demographics and demo

---
+++ Northwind-desktop-app/Models/CustomerCustomerDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind_desktop_app.Models
+{
+    class CustomerCustomerDemo
+    {
+        [Key, Column(Order = 0)]
+        public string CustomerID { get; set; }
+        [ForeignKey("CustomerID")]
+        public Customers Customers { get; set; }
+
+        [Key, Column(Order = 1)]
+        public string CustomerTypeID { get; set; }
+        [ForeignKey("CustomerTypeID")]
+        public CustomerDemographics CustomerDemographics { get; set; }
+    }
+}
