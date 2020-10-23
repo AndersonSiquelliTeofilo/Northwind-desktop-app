1983bf9d387e4ad1693ae56cc45fee04a7d3b37c
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2020-10-22 21:38:16 -0300

    Shippers and CustomerDemographics

---
+++ Northwind-desktop-app/Models/CustomerDemographics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind_desktop_app.Models
+{
+    class CustomerDemographics
+    {
+        [Key]
+        public string CustomerTypeID { get; set; }
+        public string CustomerDesc { get; set; }
+    }
+}
---
+++ Northwind-desktop-app/Models/Shippers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind_desktop_app.Models
+{
+    class Shippers
+    {
+        [Key]
+        public int ShipperID { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+    }
+}
